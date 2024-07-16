9c286e9c00fac58532567aa6101d1a1b7ebe8764
prithviseran <prithisha33@gmail.com>
2024-07-16 15:04:23 -0400

    Admin db

--- app/src/main/java/com/example/b07demosummer2024/AdminVisuals.java
+++ app/src/main/java/com/example/b07demosummer2024/AdminVisuals.java
@@ -2,11 +2,23 @@ package com.example.b07demosummer2024;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +31,14 @@ public class AdminVisuals extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://login-taam-bo7-default-rtdb.firebaseio.com/");
+    private DatabaseReference itemsRef;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TextView lotNum, itemName, itemCategory, itemPeriod;
+    private RadioButton firstRadioButtonTest;
 
     public AdminVisuals() {
         // Required empty public constructor
@@ -58,7 +74,46 @@ public class AdminVisuals extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_admin_visuals, container, false);
+
+        lotNum = view.findViewById(R.id.lot1);
+        itemName = view.findViewById(R.id.name1);
+        itemCategory = view.findViewById(R.id.category1);
+        itemPeriod= view.findViewById(R.id.description1);
+
+        firstRadioButtonTest = view.findViewById(R.id.radioButton1);
+        firstRadioButtonTest.setChecked(true);
+
+
+
+        itemsRef = database.getReference("categories");
+
+        System.out.println("Items Ref " + itemsRef);
+        //String id = itemsRef.push().getKey();
+        //Item item = new Item(lotNum, name, category, period, description, pic);
+
+
+        itemsRef.child("Test").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    lotNum.setText(String.valueOf(task.getResult().child("lotNum").getValue())); //task.getResult().child("lotNum").getValue())
+                    itemName.setText(String.valueOf(task.getResult().child("name").getValue())); //task.getResult().child("name").getValue())
+                    itemCategory.setText(String.valueOf(task.getResult().child("category").getValue())); //task.getResult().child("category").getValue())
+                    itemPeriod.setText(String.valueOf(task.getResult().child("period").getValue())); //task.getResult().child("period").getValue())
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_visuals, container, false);
+        return view;
+
+
     }
 }

424c3282dc857655f6f527cf1ad93251c1d9ee98
Jerry He <jerryhe1305@gmail.com>
2024-07-17 13:29:52 -0400

    feat: implemented database search

--- app/src/main/java/com/example/b07demosummer2024/Item.java
+++ app/src/main/java/com/example/b07demosummer2024/Item.java
@@ -33,4 +33,7 @@ public class Item {
     public void setDescription(String description) { this.description = description; }
     public String getPic() { return picURL; }
     public void setPic(String picURL) { this.picURL= picURL; }
+
+
+
 }
--- app/src/main/java/com/example/b07demosummer2024/SearchFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/SearchFragment.java
@@ -10,11 +10,18 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SearchFragment extends Fragment {
     private EditText editTextLotNum, editTextName;
@@ -22,7 +29,10 @@ public class SearchFragment extends Fragment {
     private ImageButton imageButton;
     private Button submitButton;
     private CheckBox reportCheckBox;
+    private TextView errorMsg;
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://login-taam-bo7-default-rtdb.firebaseio.com/");
+    private DataSnapshot itemsSnap;
+    private DatabaseReference itemsRef;
 
     @Nullable
     @Override
@@ -54,14 +64,62 @@ public class SearchFragment extends Fragment {
         // CheckBox
         reportCheckBox = view.findViewById(R.id.checkBox);
 
+        // Error Msg
+        errorMsg = view.findViewById(R.id.errorMsg);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do not generate report
+                if (!reportCheckBox.isChecked())
+                    searchItem();
+                // generate report
+                else{
+                    // report code here
+                }
+            }
+        });
+
         return view;
     }
 
     private void searchItem(){
-        String lotNum = editTextLotNum.getText().toString().trim();
-        String name = editTextName.getText().toString().trim();
-        String category = spinnerCategory.getSelectedItem().toString().trim();
-        String period = spinnerPeriod.getSelectedItem().toString().trim();
+        String lotNum = (editTextLotNum.getText().toString().trim()).toLowerCase();
+        String name = (editTextName.getText().toString().trim()).toLowerCase();
+        String category = (spinnerCategory.getSelectedItem().toString().trim()).toLowerCase();
+        String period = (spinnerPeriod.getSelectedItem().toString().trim()).toLowerCase();
 
-    }
+        if (lotNum.isEmpty() || name.isEmpty() || category.isEmpty() || period.isEmpty()) {
+            Toast.makeText(getContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        itemsRef = database.getReference();
+        itemsRef.child("Items").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    Item item = snapshot.getValue(Item.class);
+
+                    if (lotNum.equals(item.getLotNum().toLowerCase()) && name.equals(item.getName().toLowerCase()) && category.equals(item.getCategory().toLowerCase()) && period.equals(item.getPeriod().toLowerCase())) {
+                        // perform result of search
+                        // ...
+                        // ...
+                        errorMsg.setText("Successful Search");
+                        return;
+                    }
+
+                }
+                errorMsg.setText("Cannot find item");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        }
 }

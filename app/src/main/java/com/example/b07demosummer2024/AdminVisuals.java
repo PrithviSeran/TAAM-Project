3c4f38c3fea7ce9bf64e53d877a6b759b355c07c
prithviseran <prithisha33@gmail.com>
2024-07-17 22:55:35 -0400

    dynamic chart

--- app/src/main/java/com/example/b07demosummer2024/AdminVisuals.java
+++ app/src/main/java/com/example/b07demosummer2024/AdminVisuals.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +24,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,84 +47,86 @@ public class AdminVisuals extends Fragment {
     private String mParam2;
     private TextView lotNum, itemName, itemCategory, itemPeriod;
     private RadioButton firstRadioButtonTest;
+    private TableRow tableRow1;
 
-    /*
-    public AdminVisuals() {
-        // Required empty public constructor
-    }
+    private TextView textView1, textView2, textView3, textView4;
+    private CheckBox checkBox;
+    private Button viewItem;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminVisuals.
 
-    // TODO: Rename and change types and number of parameters
-    public static AdminVisuals newInstance(String param1, String param2) {
-        AdminVisuals fragment = new AdminVisuals();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-    */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
         View view = inflater.inflate(R.layout.fragment_admin_visuals, container, false);
 
-        lotNum = view.findViewById(R.id.lot1);
-        itemName = view.findViewById(R.id.name1);
-        itemCategory = view.findViewById(R.id.category1);
-        itemPeriod= view.findViewById(R.id.description1);
+        TableLayout tableLayout1 = view.findViewById(R.id.tableLayout);
 
 
-        //firstRadioButtonTest = view.findViewById(R.id.radiobutton1);
+        itemsRef = database.getReference("TestBranch");
 
 
-
-
-        itemsRef = database.getReference("categories");
-
-        System.out.println("Items Ref " + itemsRef);
-        //String id = itemsRef.push().getKey();
-        //Item item = new Item(lotNum, name, category, period, description, pic);
-
-
-        itemsRef.child("Test").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        itemsRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
 
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (!task.isSuccessful()) {
                     Log.e("firebase", "Error getting data", task.getException());
                 }
                 else {
-                    lotNum.setText(String.valueOf(task.getResult().child("lotNum").getValue())); //task.getResult().child("lotNum").getValue())
-                    itemName.setText(String.valueOf(task.getResult().child("name").getValue())); //task.getResult().child("name").getValue())
-                    itemCategory.setText(String.valueOf(task.getResult().child("category").getValue())); //task.getResult().child("category").getValue())
-                    itemPeriod.setText(String.valueOf(task.getResult().child("period").getValue())); //task.getResult().child("period").getValue())
+
+                    Log.d("Class", String.valueOf(task.getResult().getValue().getClass()));
+
+
+
+                    for (Object entry1 : ((HashMap)task.getResult().getValue()).entrySet()) {
+                        tableRow1 = new TableRow(getActivity());
+                        checkBox = new CheckBox(getActivity());
+
+                        tableRow1.addView(checkBox);
+
+                        for (Object entry2 : ((HashMap)((Map.Entry)entry1).getValue()).entrySet()) {
+                            System.out.println("Key: " + ((Map.Entry)entry2).getKey() + ", Value: " + ((Map.Entry)entry2).getValue());
+
+                            // Create new TextView
+                            textView1 = new TextView(getActivity());
+                            textView1.setText(String.valueOf(((Map.Entry)entry2).getValue()));
+
+
+                            tableRow1.addView(textView1);
+
+
+                        }
+                        viewItem = new Button(getActivity());
+                        viewItem.setText("View Item");
+
+                        viewItem.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // Handle the button click
+                                // loadFragment(new ViewItem( .... ));
+                            }
+                        });
+
+                        tableRow1.addView(viewItem);
+
+
+                        tableLayout1.addView(tableRow1);
+                    }
+
+
                     Log.d("firebase", String.valueOf(task.getResult().getValue()));
                 }
             }
         });
 
-
         // Inflate the layout for this fragment
         return view;
 
-
+    }
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }

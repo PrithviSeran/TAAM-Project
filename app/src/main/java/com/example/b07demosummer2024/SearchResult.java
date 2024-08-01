2878fde90c493a08557fabbb2318dd80cc6f68f9
Jerry He <jerryhe1305@gmail.com>
2024-07-31 20:52:39 -0400

    fixed soft keyboard bug in keyword search, and view items resulting in null inside search results

--- app/src/main/java/com/example/b07demosummer2024/SearchResult.java
+++ app/src/main/java/com/example/b07demosummer2024/SearchResult.java
@@ -71,7 +71,7 @@ public class SearchResult extends TAAMSFragment implements ViewItemsTable {
                 @Override
                 public void onClick(View v) {
                     // Handle the button click
-                    loadFragment(new ViewItem(item.getName()));
+                    loadFragment(new ViewItem(item.getLotNum()));
                 }
             });
 
